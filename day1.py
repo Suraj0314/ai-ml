0b0c44416ef7c5cb191cfe32cffd3110c2316fb1
Suraj <surajprasad0406@gmail.com>
2026-03-26 23:17:39 +0530

    Day 1 final improved version

--- day1.py
+++ day1.py
@@ -1,18 +1,36 @@
 numbers = []
-for i in range(5):
-    user = int(input("Enter number: "))
-    numbers.append(user)
+
+while True:
+    num = input("Enter number or done: ")
+    
+    try:
+        if num.lower() == "done":
+            break
+        else:
+            numbers.append(int(num))    
+    except ValueError:
+        print("Invalid input! Try again.")
+
+if len(numbers) == 0:
+    print("No numbers entered.")
+    exit()
+
+total = sum(numbers)
+avg = total / len(numbers)
+
 print(f"Largest: {max(numbers)}")
 print(f"Smallest: {min(numbers)}")
-print(f"Sum: {sum(numbers)}")
-print(f"Average: {sum(numbers) / len(numbers)}")
+print(f"Sum: {total}")
+print(f"Average: {avg}")
 
 even = 0
 odd = 0
+
 for i in numbers:
     if i % 2 == 0:
         even += 1
-    if i % 2 != 0:
-        odd += 1 
+    else:
+        odd += 1
+
 print(f"Even numbers: {even}")
-print(f"Odd numbers: {odd}")           
+print(f"Odd numbers: {odd}")
